4fa58b2a1d095646ccb5c9a91346d6863881c5a3
sarika20 <36072134+sarika20@users.noreply.github.com>
2025-07-24 11:43:49 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "web" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  tags = {
+    Name = "cloud-native-web"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update -y",
+      "sudo apt install nginx -y"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file(var.private_key_path)
+      host        = self.public_ip
+    }
+  }
+}
